15144f0e1e56d311c906abedcbd3d3ba3d2b5cac
Michael Mattocks <mike.mattocks@utoronto.ca>
2021-03-04 02:48:22 -0500

    updated spike recovery, readme

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-## BioMotifInference
+# BioMotifInference
 [![Build Status](https://travis-ci.org/mmattocks/BioMotifInference.jl.svg?branch=master)](https://travis-ci.org/mmattocks/BioMotifInference.jl)
 [![codecov](https://codecov.io/gh/mmattocks/BioMotifInference.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/mmattocks/BioMotifInference.jl)
-[![Project Status: WIP – Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.](https://www.repostatus.org/badges/latest/wip.svg)](https://www.repostatus.org/#wip)
+[![Project Status: WIP – Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.](https://www.repostatus.org/badges/latest/inactive.svg)](https://www.repostatus.org/#inactive)
+
+BioMotifInference.jl implements nested sampling of ensembles of Independent Component Analysis (ICA) models. These are composed of Position Weight Matrix (PWM) DNA motif signals emitting on a background Hidden Markov Model of genomic noise (BHMM). It is intended for Distributed.jl cluster use, although smaller problems can be solved on one machine. BHMMs can be trained using [BioBackgroundModels.jl](https://github.com/mmattocks/BioBackgroundModels.jl). BioMotifInference.jl is similar to nMICA, implemented by Down and Hubbard in Java in 2004 (Down, T. A., and Tim J.P. Hubbard. “NestedMICA: Sensitive Inference of over-Represented Motifs in Nucleic Acid Sequence.” Nucleic Acids Research 33, no. 5 (March 8, 2005): 1445–53. https://doi.org/10.1093/nar/gki282.
+).
+
+# Installation
+
+Add the github url using the Julia package manager:
+
+```
+julia>]add https://github.com/mmattocks/BioMotifInference.jl
+```
+
+# Example use
+
+A synthetic example, using observations generated from a background model of _D. rerio_ intergenic sequences, with both a periodic CTC motif and an irregular, but recurring TATAWAW box spiked in, can be found in tests/spike recovery.jl.
+
+'''
+include(dirname(dirname(pathof(BioMotifInference)))*"/test/spike_recovery.jl")
+'''
+
+Output:
+
+![](bmi.gif)
+
+# Documentation
+More extensive documentation is available [here](https://github.com/mmattocks/Thesis/blob/master/chapters/PTII/BMI.tex).
--- test/spike_recovery.jl
+++ test/spike_recovery.jl
@@ -1,20 +1,24 @@
 #Testbed for synthetic spike recovery from example background
-
-using nnlearn, BGHMM, HMMBase, Distributions, Random, Serialization, Distributions
-
+using BioBackgroundModels, BioMotifInference, Random, Distributed, Distributions, Serialization
 Random.seed!(786)
-
 #CONSTANTS
-no_obs=1000
-obsl=140
+no_obs=500
+obsl=100:200
 
-hmm=HMM{Univariate,Float64}([0.4016518533961019, 0.2724399569450827, 0.3138638675018568, 0.012044322156962559], [3.016523036789942e-9 2.860631288328858e-6 0.2299906524188302 0.7700064839333549; 3.0102278323431375e-11 0.7494895424906354 0.23378615437778671 0.016724303101477486; 8.665894321573098e-17 0.2789950381410553 0.7141355461949104 0.006869415664033568; 0.006872526597796038 0.016052425322133648 0.017255179541768192 0.9598198685383041], [Categorical([0.1582723599684065, 0.031949729618356536, 0.653113286526948, 0.15666462388628763]), Categorical([0.4610499748798372, 0.2613013005680122, 0.15161801872560146, 0.12603070582654768]), Categorical([0.08601960130086236, 0.13869192872427524, 0.26945182329686523, 0.5058366466779973]), Categorical([0.3787366527613563, 0.11034604356978756, 0.1119586976058099, 0.3989586060630392])])
+const folder_path="/bench/PhD/NGS_binaries/BBM/refined_folders"
 
-struc_sig=[.1 .7 .1 .1
+report_folders=deserialize(folder_path)
+
+bhmm_vec=[BHMM([0.40027008799648645, 0.1997894691941005, 0.006556173444055311, 0.3381222117369811, 0.053302496489949405, 0.0019595611384158307], [0.9910385961237116 0.001055084535731191 0.0030117366072612726 0.0025443692116936516 1.875366316590218e-83 0.002350213521605879; 0.003592562092379169 0.6212087435679351 1.4862557155404555e-91 0.3738436265294334 0.001355067810251799 5.674610926019336e-174; 0.03314627345971247 0.00026886477654034587 0.9628685354878737 3.677787936178394e-5 0.00367954839651186 5.0e-324; 0.0002903769304330447 0.3737391779744011 1.584645416437449e-35 0.6225645652084915 0.002839228767872827 0.0005666511188010834; 0.0005590565306708014 0.0064195824547637215 0.004061509889975232 0.01962586251822172 0.9687912312979926 0.0005427573083774576; 0.0174697691356862 2.0648278441425858e-20 8.008498087945918e-218 0.004151185943888868 0.0013251598095032858 0.9770538851109234], [Categorical([0.2565257692018922, 0.24506145230281004, 0.2649485969649606, 0.23346418153033835]), Categorical([0.052491168994042534, 0.0987036876775239, 0.2383241923003866, 0.610480951028046]), Categorical([0.08371833024203595, 0.07204591736665274, 0.3854013598343639, 0.4588343925569475]), Categorical([0.5954709485303519, 0.2497223085607694, 0.09528006039357192, 0.0595266825153071]), Categorical([0.4744103622376032, 0.04038264172760258, 0.04588736323302444, 0.4393196328017719]), Categorical([0.41173384083242826, 0.44270375576246973, 0.030612125665857307, 0.11495027773924743])])]
+
+bhmm_dist=Categorical([1.])
+
+struc_sig_1=[.1 .7 .1 .1
            .1 .1 .1 .7
            .1 .7 .1 .1]
 periodicity=8
-struc_frac_obs=.35
+struc_frac_obs=.75
+
 
 tata_box=[.05 .05 .05 .85
           .85 .05 .05 .05
@@ -23,146 +27,39 @@ tata_box=[.05 .05 .05 .85
           .425 .075 .075 .425
           .85 .05 .05 .05
           .425 .075 .075 .425]
-tata_frac_obs=.7
-tata_recur_range=1:4
+motif_frac_obs=.7
+motif_recur_range=1:4
 
-combined_ensemble = "/bench/PhD/NGS_binaries/nnlearn/combined_ensemble"
+@info "Constructing synthetic sample set..."
+obs1, bg_scores1, hmm_truth1, spike_truth1 = synthetic_sample(no_obs,obsl,bhmm_vec,bhmm_dist,[struc_sig_1,tata_box],[(true,(struc_frac_obs,periodicity)),(false,(motif_frac_obs,motif_recur_range))])
+
+e1 = "BMISpikeTest"
 
 #JOB CONSTANTS
-const position_size = 141
-const ensemble_size = 100
-const no_sources = 3
+const ensemble_size = 750
+const no_sources = 4
 const source_min_bases = 3
 const source_max_bases = 12
-@assert source_min_bases < source_max_bases
 const source_length_range= source_min_bases:source_max_bases
-const mixing_prior = .3
-@assert mixing_prior >= 0 && mixing_prior <= 1
+const mixing_prior = .5
 const models_to_permute = ensemble_size * 3
+funcvec=full_perm_funcvec
+push!(funcvec, BioMotifInference.permute_source)
+push!(funcvec, BioMotifInference.permute_source)
+args=[Vector{Tuple{Symbol,Any}}() for i in 1:length(funcvec)]
+args[end-1]=[(:weight_shift_freq,0.),(:length_change_freq,1.),(:length_perm_range,1:1)]
+args[end]=[(:weight_shift_freq,.1),(:length_change_freq,0.),(:weight_shift_dist,Uniform(.00001,.01))]
 
-job_sets=[
-([
-    ("PS", (no_sources)),
-    ("PM", (no_sources)),
-    ("PSFM", (no_sources)),
-    ("PSFM", (no_sources*10, .8, 1.)),
-    ("FM", ()),
-    ("DM", (no_sources)),
-    ("SM", (no_sources)),
-    ("RD", (no_sources)),
-    ("RI", (no_sources)),
-    ("EM", (no_sources))
-],[.025, .025, .025, .025, .775, .025, .025, .025, .025, .025]),
-([
-    ("PS", (no_sources)),
-    ("PM", (no_sources)),
-    ("PSFM", (no_sources)),
-    ("PSFM", (no_sources, 0., 1.)),
-    ("PSFM", (no_sources, 0.8, .0)),
-    ("FM", ()),
-    ("DM", (no_sources)),
-    ("SM", (no_sources)),
-    ("RD", (no_sources)),
-    ("RI", (no_sources*10)),
-    ("EM", (no_sources))
-],[.05, .15, .15, .10, .05, .20, 0.0, 0.05, 0.15, 0.10, .0]),
-]
-job_limit=4
 
-const prior_wt=1.1
-
-#FUNCTIONS
-function setup_obs(hmm, no_obs, obsl)
-    obs=vec(Int64.(rand(hmm,obsl)[2]))
-    for o in 2:no_obs
-        obs=hcat(obs, vec(Int64.(rand(hmm,obsl)[2])))
-    end
-    obs=vcat(obs,zeros(Int64,1,no_obs))
-    return obs
-end
-
-function spike_irreg!(obs, source, frac_obs, recur)
-    truth=Vector{Int64}()
-    for o in 1:size(obs,2)
-        if rand()<frac_obs
-            push!(truth, o)
-            for r in rand(recur)
-                rand()<.5 && (source=nnlearn.revcomp_pwm(source))
-                pos=rand(1:size(obs,1)-1)
-                pwm_ctr=1
-                while pos<=size(obs,1)-1&& pwm_ctr<=size(source,1)
-                    obs[pos,o]=rand(Categorical(source[pwm_ctr,:]))
-                    pos+=1
-                    pwm_ctr+=1
-                end
-            end
-        end
-    end
-    return truth
-end
-
-function spike_struc!(obs, source, frac_obs, periodicity)
-    truth=Vector{Int64}()
-    truthpos=Vector{Vector{Int64}}()
-    for o in 1:size(obs,2)
-        if rand()<frac_obs
-            push!(truth, o)
-            rand()<.5 && (source=nnlearn.revcomp_pwm(source))
-            pos=rand(1:periodicity)
-            posvec=Vector{Int64}()
-            while pos<=size(obs,1)
-                pos_ctr=pos
-                pwm_ctr=1
-                while pos_ctr<=size(obs,1)-1&&pwm_ctr<=size(source,1)
-                    push!(posvec,pos_ctr)
-                    base=rand(Categorical(source[pwm_ctr,:]))
-                    obs[pos_ctr,o]=base
-                    pos_ctr+=1
-                    pwm_ctr+=1
-                end
-                pos+=periodicity+pwm_ctr
-            end
-            push!(truthpos,posvec)
-        end
-    end
-    return truth,truthpos
-end
-
-function get_BGHMM_lhs(obs,hmm)
-    lh_mat=zeros(size(obs,1)-1,size(obs,2))
-    for o in 1:size(obs,2)
-        obso=zeros(Int64,size(obs,1),1)
-        obso[1:end,1] = obs[:,o]
-        lh_mat[:,o]=BGHMM.get_BGHMM_symbol_lh(Matrix(transpose(obso)),hmm)
-    end
-    return lh_mat
-end
-
-@info "Setting up synthetic observation set..."
-obs=setup_obs(hmm, no_obs, obsl)
-struc_truth,struc_postruth=spike_struc!(obs, struc_sig, struc_frac_obs, periodicity)
-irreg_truth=spike_irreg!(obs, tata_box, tata_frac_obs, tata_recur_range)
-
-@info "Calculating background likelihood matrix..."
-bg_lhs=get_BGHMM_lhs(obs,hmm)
+instruct = Permute_Instruct(funcvec, ones(length(funcvec))./length(funcvec),models_to_permute,100; args=args)
 
 @info "Assembling source priors..."
-#prior_array= [struc_sig, tata_box]
 prior_array= Vector{Matrix{Float64}}()
-source_priors = nnlearn.assemble_source_priors(no_sources, prior_array, prior_wt, source_length_range)
+source_priors = BioMotifInference.assemble_source_priors(no_sources, prior_array)
 
 @info "Assembling ensemble..."
-path=randstring()
-isfile(string(path,'/',"ens")) ? (ens = deserialize(string(path,'/',"ens"))) :
-    (ens = nnlearn.IPM_Ensemble(path, ensemble_size, source_priors, (falses(0,0), mixing_prior), bg_lhs, obs, source_length_range))
-
-
-job_set_thresh=[-Inf,ens.naive_lh]
-param_set=(job_sets,job_set_thresh,job_limit)
+isfile(e1*"/ens") ? (ens1 = deserialize(e1*"/ens")) :
+    (ens1 = IPM_Ensemble(e1, ensemble_size, source_priors, (falses(0,0), mixing_prior), bg_scores1, obs1, source_length_range))
     
 @info "Converging ensemble..."
-nnlearn.ns_converge!(ens, param_set, models_to_permute, .0001, model_display=no_sources, backup=(true,5))
-
-rm(path,recursive=true)
-
-#811973
+logZ1 = BioMotifInference.converge_ensemble!(ens1, instruct, backup=(true,250), wk_disp=false, tuning_disp=false, ens_disp=true, conv_plot=false, src_disp=true, lh_disp=false, liwi_disp=false)
